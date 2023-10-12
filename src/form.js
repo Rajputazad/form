08ab8effd74fec88468e6b604fe71e249221842d
Rajputazad <Rajputazad337@gmail.com>
2023-10-12 13:53:15 +0530

    react form

--- src/form.js
+++ src/form.js
@@ -1,27 +1,90 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
+export default function Form() {
+  const [nameerror,setnameerror]=useState();
+  const [emailerror,setemailerror]=useState();
 
-export default function Form(){
-// const [formdata,formstate]=useState({
-//     name:"",
-//     email:""
-// })
+  const [formdata, setFormdata] = useState({
+    name: "",
+    email: "",
+  });
 
+  const namehandle = (e) => {
 
-    return(
-        <div>
-        <form>
-          <lable>Name:</lable>
-          <br />
-          <input name="name" />
-          <br />
-          <lable>Email:</lable>
-          <br />
-          <input name="email"/>
-      {/* <Button variant="contained" color="primary">
-        submit
-    </Button> */}
-        </form>
-      </div>
-    )
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormdata({ ...formdata, [name]: value });
+    if(value.length<3){
+      setnameerror(true)
+    } else{
+      
+      setnameerror(false)
+      
+    }
+    // console.log(error);
+    
+  };
+  const emailhandle = (e) => {
+    // console.log(error);
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormdata({ ...formdata, [name]: value });
+    if(value.length<3){
+      setemailerror(true)
+      
+      
+    }else{
+      // setstatus(true)
+      setemailerror(false)
+    
+    }
+
+    // return false
+  };
+
+  const submit = (e) => {
+    console.log(formdata.name.length<3);
+    e.preventDefault()
+    if(formdata.name.length<3 || formdata.email.length<3){
+      setemailerror(true)
+      alert("error")
+    }else{
+      alert("name:-"+formdata.name+" email:-"+formdata.email)
+      console.log(formdata);
+      
+  }
+}
+  return (
+    <div className="row">
+      <form onSubmit={submit}>
+        <label>Name:</label>
+        <br />
+        <input 
+          placeholder="name"
+          name="name"
+          
+          onChange={namehandle}
+          type="text"
+          value={formdata.name}
+        /><br/>{nameerror?<span>Name is not valid</span>:null}  
+      
+        <br />
+        <br />
+        <label>Email:</label>
+        <br />
+        <input autoComplete="false"
+          placeholder="email"
+          onChange={emailhandle}
+          name="email"
+          type="email"
+          value={formdata.email}
+        /><br/>{emailerror?<span>Email is not valid</span>:null} 
+        <br />
+        <br />
+        <button disabled={emailerror==true || nameerror==true } type="submit" className="btn btn-primary">
+          Click me
+        </button>
+      </form>
+    </div>
+  );
 }
